9646e41920cfb20f80b0262bd5b6f5a5c8eedfc4
Chase Monico <47125081+cmonico1@users.noreply.github.com>
2019-04-15 19:25:32 -0400

    jQuery Hover State changed to mouse enter
    Created mouse enter and mouse leave to truncate the hover state and increase page speed.

--- js/modal.js
+++ js/modal.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
   /*Hover States*/
   /* Brad Frost */
-  $("#speakers_flex_1").hover(function(){ /*On hover shows hover state, and then returns to original html/css */
+  $("#speakers_flex_1").mouseenter(function(){ /*On hover shows hover state, and then returns to original html/css */
     $("#speakers_flex_hover_1").html("<p>Brad Frost</p>").css(".speakers_hover_on");
-  }, function(){
-    $("#speakers_flex_hover_1").html("<p></p>").css("display": "none");
   });
+  $("#speakers_flex_1").mouseleave(function(){ /*On hover shows hover state, and then returns to original html/css */
+    $("#speakers_flex_hover_1").html('<img src="../img/brad_frost.jpg" class="speakers_flex_img" id="speakers_flex_img_1" alt="DevCon Logo">');
+  });
+
   /* Modal State */
   $("#speakers_flex_1").click(function(){
     $("#speakers_modal_content_1").css({
@@ -24,23 +26,13 @@ $(document).ready(function(){
   });
 
   /* Cal Henderson */
-  $("#speakers_flex_2").hover(function(){
-    $("#speakers_flex_hover_2").html("<p>Cal Henderson</p>").css({
-      "width": "247px",
-      "height": "247px",
-      "border-radius": "50%",
-      "background-color": "#e83d4d",
-    });
-    $(".speakers_flex_hover p").css({"font-size": "45px", "padding": "40px 0"});
-  }, function(){
-    $("#speakers_flex_hover_2").html('<img src="../img/cal_henderson.jpg" class="speakers_flex_img" id="speakers_flex_img_1" alt="DevCon Logo">').css({
-      "width": "247px",
-      "height": "247px",
-      "border-radius": "50%",
-      "background-color": "transparent",
-    });
-    $(".speakers_flex_hover p").css({"font-size": "64px", "padding": "25px 0"});
+  $("#speakers_flex_1").mouseenter(function(){ /*On hover shows hover state, and then returns to original html/css */
+    $("#speakers_flex_hover_2").html("<p>Cal Henderson</p>").css(".speakers_hover_on");
   });
+  $("#speakers_flex_1").mouseleave(function(){ /*On hover shows hover state, and then returns to original html/css */
+    $("#speakers_flex_hover_2").html('<img src="../img/cal_henderson.jpg" class="speakers_flex_img" id="speakers_flex_img_1" alt="DevCon Logo">');
+  });
+
   /* Modal State */
   $("#speakers_flex_2").click(function(){
     $("#speakers_modal_content_2").css({
