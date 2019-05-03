414ef84047d20a4055270598ba87b38113f90f19
Chase <chasemonicodesign@gmail.com>
2019-05-03 15:54:50 -0400

    Correction of Files
    Deleting and reuploading files to correct issue with multile files generating.

--- README.md
+++
@@ -1,9 +0,0 @@
-# YCP Tech Conference Project
-
-Web Design 2 semester long responsive web design project. 
-What you'll find in here is a... 
-- HTML
-- CSS
-- JS
-- Associated images and other files
-- For Spring 2019 Web Design 2
--- js/legal.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $("#legal_dropdown_bar_1").click(function(){
-    $("#legal_dropdown_1").slideToggle();
-  });
-  $("#legal_dropdown_bar_2").click(function(){
-    $("#legal_dropdown_2").slideToggle();
-  });
-});
